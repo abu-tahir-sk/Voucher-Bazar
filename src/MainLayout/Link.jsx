f1e3df468aaf58e1af56d2b70cdb4fb48281aaec
abu-tahir-sk <abutahir2626@gmail.com>
2025-10-31 20:16:17 -0900

    navbar responsive

---
+++ src/MainLayout/Link.jsx
@@ -0,0 +1,11 @@
+
+
+const Link = ({route}) => {
+      return (
+            <li>
+                  
+            </li>
+      );
+};
+
+export default Link;
--- src/MainLayout/Navbar/Navbar.jsx
+++ src/MainLayout/Navbar/Navbar.jsx
@@ -1,45 +1,41 @@
 import { useState } from "react";
+import { RiCloseLargeFill } from "react-icons/ri";
+import { TiThMenu } from "react-icons/ti";
 import { Link, NavLink } from "react-router-dom";
 
-
 const Navbar = () => {
-  const [menuOpen,setMenuOpen]=useState(false)
+  
 
-  const link = (
-    <>
-      <li>
-        <a>Home</a>
-      </li>
-      <li>
-        <a>Brands</a>
-      </li>
-      <li>
-        <a>My Profile</a>
-      </li>
-      <li>
-        <a>About Dev</a>
-      </li>
-    </>
-  );
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="flex justify-between items-center px-8 bg-[#ffe8e4] h-16">
-      <div>
-        <h2 className="text-[24px] font-extrabold text-[#2b1c1c] inter-font">Voucher<span className="text-[#FF6D00]">BD</span></h2>
+      <div className="flex justify-start items-center gap-2 w-full">
+        <div className="text-3xl md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <RiCloseLargeFill /> : <TiThMenu />}
+        </div>
+        <h2 className="text-[26px] font-extrabold text-[#2b1c1c] inter-font">
+          Voucher<span className="text-[#FF6D00]">BD</span>
+        </h2>
       </div>
-     
-       <ul className="lg:flex justify-center items-center  gap-4">
-        <li><NavLink>Home</NavLink></li>
-        <li><NavLink>Brands</NavLink></li>
-        <li><NavLink>my-profile</NavLink></li>
-        <li><NavLink>About Dev</NavLink></li>
+
+      <ul className={
+        `md:flex justify-center duration-1000 items-center text-[14px] gap-4 bg-[#ffe8e4] p-3 w-full  md:h-16 absolute md:static ${menuOpen ? "top-[64px] left-0":"-top-32"}`
+      }>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/brands">Brands</NavLink></li>
+        <li><NavLink to="/myProfile">My Profile</NavLink></li>
+        <li><NavLink to="/aboutDev">About Dev</NavLink></li>
+        
+       
       </ul>
-    
-      <div className="flex justify-center items-center gap-4">
-        <button>
+
+      <div className="flex justify-end items-center gap-4 w-full">
+        <button className="bg-[#FF6D00] btn text-white">
           {" "}
           <Link to="/login">Login</Link>
         </button>
-        <button>
+        <button className="bg-[#f6e4d2] btn text-[#000000] border-2 border-[#FF6D00]">
           <Link to="/signUp">sign Up</Link>
         </button>
       </div>
